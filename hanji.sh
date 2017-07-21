4a68fba12790a4b568a9a948f838402536d89672
Josiah Purtlebaugh <josiah.purtlebaugh@gmail.com>
2017-07-21 11:44:29 +0900

    Update startup script to add start/stop

--- hanji.sh
+++ hanji.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-nohup java -Dprofiles=submodules/language-detection/profiles -jar build/libs/hanji-0.1.jar 2>hanji-error.log 1>hanji.log &
+if [ "$1" == "start" ]; then 
+  nohup java -Dhanji.profilesDir=submodules/language-detection/profiles -jar build/libs/hanji-0.2.jar 2>hanji-error.log 1>hanji.log & echo $! > hanji.pid
+elif [ "$1" == "stop" ]; then
+  kill `cat hanji.pid`
+fi 
